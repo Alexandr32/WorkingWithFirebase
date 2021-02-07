3ea0282395ae5779d7ca6817c51e5f5c07b2cf5e
Alex <alex3289.aa@gmai.com>
2021-02-07 18:37:12 +0500

    Отдельные компоненты

--- GameWeb/src/app/app-routing.module.ts
+++ GameWeb/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StartComponent } from './start/start.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: StartComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+  
 })
 export class AppRoutingModule { }
--- GameWeb/src/app/app.module.ts
+++ GameWeb/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { StartComponent } from './start/start.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    StartComponent
   ],
   imports: [
     BrowserModule,
